a5d67573bc57c0adf6a6dc7a17395604b4598308
Q. Price <priceqp@gmail.com>
2019-07-01 22:36:49 -0500

    Add remaining cards cound and disable button when no cards are left

--- src/Deck.js
+++ src/Deck.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 class Deck extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { deck: '', card: '', altContent: '' };
+        this.state = { deck: '', card: '', altContent: '', remaining: '' };
         this.handleClick = this.handleClick.bind(this);
     }
     async componentDidMount() {
@@ -15,17 +15,18 @@ class Deck extends React.Component {
         console.log(response);
     }
     async handleClick() {
-        let response = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck}/draw/`);
-        let altContent = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`;
-        let card = response.data.cards[0].image;
-        this.setState({ card, altContent });
-        console.log(altContent);
+        let response = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck}/draw/`),
+            altContent = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`,
+            card = response.data.cards[0].image,
+            remaining = response.data.remaining;
+        this.setState({ card, altContent, remaining });
     }
     render() {
         return(
             <div>
                 <h1>♠︎ Card Dealer ♠︎</h1>
-                <button onClick={this.handleClick}>Hit Me!</button>
+                <h3>Cards Remaining: {this.state.remaining}</h3>
+                <button onClick={this.handleClick} disabled={this.state.remaining === 0}>Hit Me!</button>
                 <Card card={this.state.card} altContent={this.state.altContent} />
             </div>
         );
