581dbb53f5e21ab8d1b11b6711004f8c16d5304d
Q. Price <priceqp@gmail.com>
2019-07-01 21:47:40 -0500

    Add state and initial API request to deck component

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 class Deck extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { deck: '' };
+    }
+    async componentDidMount() {
+        let response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        let deck = response.data.deck_id;
+        this.setState({ deck });
+      }
     render() {
         return(
             <div>
+                <h1>♠︎ Card Dealer ♠︎</h1>
+                <button>Hit Me!</button>
                 <h3>Render Cards Here</h3>
                 <Card />
             </div>
