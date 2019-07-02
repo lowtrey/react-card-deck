7143ed818f272318badea1d2eb7011c80b47fc40
Q. Price <priceqp@gmail.com>
2019-07-01 22:23:19 -0500

    Create handleClick function that gets random card from API, render card image below button

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Card extends React.Component {
     render() {
         return(
-            <h5>Card</h5>
+            <div>
+                <img src={this.props.card} alt={this.props.altContent} />
+            </div>
         );
     }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -5,20 +5,28 @@ import axios from 'axios';
 class Deck extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { deck: '' };
+        this.state = { deck: '', card: '', altContent: '' };
+        this.handleClick = this.handleClick.bind(this);
     }
     async componentDidMount() {
         let response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
         let deck = response.data.deck_id;
         this.setState({ deck });
-      }
+        console.log(response);
+    }
+    async handleClick() {
+        let response = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck}/draw/`);
+        let altContent = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`;
+        let card = response.data.cards[0].image;
+        this.setState({ card, altContent });
+        console.log(altContent);
+    }
     render() {
         return(
             <div>
                 <h1>♠︎ Card Dealer ♠︎</h1>
-                <button>Hit Me!</button>
-                <h3>Render Cards Here</h3>
-                <Card />
+                <button onClick={this.handleClick}>Hit Me!</button>
+                <Card card={this.state.card} altContent={this.state.altContent} />
             </div>
         );
     }
