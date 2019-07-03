0d2fdce25c655fb2b7fb564e7ee3b22c6d7dcc63
Q. Price <priceqp@gmail.com>
2019-07-02 20:12:44 -0500

    Code refactor, change state syntax and getCard function

--- src/Deck.js
+++ src/Deck.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import Card from './Card';
 import axios from 'axios';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Deck extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { deck: '', card: '', altContent: '', remaining: '' };
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { deck: null, drawn: [], altContent: '', remaining: '' };
+        this.getCard = this.getCard.bind(this);
     }
     async componentDidMount() {
-        let response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-        let deck = response.data.deck_id;
-        this.setState({ deck });
-        console.log(response);
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/?deck_count=1`);
+        this.setState({ deck: deck.data });
     }
-    async handleClick() {
-        let response = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck}/draw/`),
-            altContent = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`,
-            card = response.data.cards[0].image,
-            remaining = response.data.remaining;
-        this.setState({ card, altContent, remaining });
+    async getCard() {
+        let id = this.state.deck.deck_id;
+        try {
+            let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+            let cardRes = await axios.get(cardUrl);
+            if(!cardRes.data.success) {
+                throw new Error('No cards remaining!')
+            }
+            let card = cardRes.data.cards[0];
+            this.setState(st => ({
+                drawn: [
+                    ...st.drawn,
+                    {
+                        id: card.code, 
+                        image: card.image,
+                        name: `${card.value} OF ${card.suit}`
+                    }
+                ]
+            }));
+        } catch (err) {
+            alert(err);
+        }
     }
     render() {
         return(
             <div>
                 <h1>♠︎ Card Dealer ♠︎</h1>
                 <h3>Cards Remaining: {this.state.remaining}</h3>
-                <button onClick={this.handleClick} disabled={this.state.remaining === 0}>Hit Me!</button>
+                <button onClick={this.getCard} disabled={this.state.remaining === 0}>Hit Me!</button>
                 <Card card={this.state.card} altContent={this.state.altContent} />
             </div>
         );
